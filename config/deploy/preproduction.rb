3b39af180c7ed449a78ccd89b591c368c57947fb
bsd-mfamilia <mvp.manuelfamilia@gmail.com>
2020-03-12 18:34:48 -0400

    Port specification for pre-production environment

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'preahoranostoca', user: 'decidim', roles: %w{app db web}
+server 'preahoranostoca', user: 'decidim', roles: %w{app db web}, port: 22221
 set branch: "master"
 set :rails_env, "production"
 
