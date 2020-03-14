599fefc2bf1601cf7ea4834ce061d2dd6ea65a5a
bsd-mfamilia <mvp.manuelfamilia@gmail.com>
2020-03-14 13:57:57 -0400

    update delayed_job_cron.sh

--- config/delayed_job_cron.sh
+++ config/delayed_job_cron.sh
@@ -2,7 +2,7 @@
 
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-APP_PATH="$HOME/decidim-app"
+APP_PATH="$HOME/decidim-app/current"
 
 if ! [ -s $APP_PATH/tmp/pids/delayed_job.pid ]; then
 	  RAILS_ENV=production $APP_PATH/bin/delayed_job start
