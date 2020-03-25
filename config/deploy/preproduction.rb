212b548bf42f7f9be26c8d19f5fc6d986ac8241e
jordibari <jordi@ciudadaniai.org>
2020-03-25 18:12:06 -0300

    change deployed branch in staging env

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '178.63.67.157', user: 'decidim', roles: %w{app db web}, port: 22221
-set branch: "master"
+set branch: "add_term_customizer"
 set :rails_env, "production"
 
 
