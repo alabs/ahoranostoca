45526fa05ab981955e67596c1451cd3de9c8c41f
jordibari <jordi@ciudadaniai.org>
2020-04-13 15:05:53 -0400

    change deployment path to current

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, "git@github.com:alabs/ahoranostoca.git"
 
 # Deploy to the user's home directory
 # set :deploy_to, "/home/deploy/#{fetch :application}"
-set :deploy_to, "/home/decidim/decidim-app"
+set :deploy_to, "/home/decidim/decidim-app/current"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 append :linked_files, 'config/application.yml'
