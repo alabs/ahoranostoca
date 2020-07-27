ff1c239c60e92fdae30b1446617364201e4acb68
daquiroz <daquiroz@uc.cl>
2020-07-27 10:06:16 -0400

    text in a territory hide

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).ready(function(){
+  $("div.floating-helper-container").addClass("hide");
+  var pathURL = $(location).attr("pathname");
+  if (pathURL == "/assemblies"){
+    $("div.floating-helper-container").removeClass("hide");
+  }
+});
